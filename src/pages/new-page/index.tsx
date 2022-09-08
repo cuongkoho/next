43bb10a26c6f2cbfa666d7bbef165f8b165a83a5
Cuong Tran <cuong@koho.ca>
2022-09-08 14:16:45 -0400

    resolve build issue

--- src/pages/new-page/index.tsx
+++ src/pages/new-page/index.tsx
@@ -5,7 +5,21 @@ export default () => {
   return (
     <>
       <h1>This page is statically rendered</h1>
+      <p>{`There's no external data, this could be used for marketing pages`}</p>
+      <p>{`Nextjs also support dynamic data page,
+          data will be fetched at build time`}</p>
       <NextLink href="/">Home Page</NextLink>
     </>
   );
 };
+
+// This gets called at build time
+export async function getStaticProps({ params }: { params: any }) {
+  //setTimeout(() => console.log("building....."), 2000);
+  //const res = await fetch(`https://.../posts/${params?.id}`);
+  //const post = await res.json();
+
+  // Pass post data to the page via props
+  //return { props: { post } };
+  return { props: {} };
+}
--- src/pages/new-ssr-page/index.tsx
+++ src/pages/new-ssr-page/index.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import NextLink from "next/link";
 
 export default () => {
-  return <h1>This page is pre-rendered</h1>;
+  return (
+    <>
+      <h1>This page is server-side rendered</h1>
+      <NextLink href="/">Home Page</NextLink>
+    </>
+  );
 };
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  //setTimeout(() => console.log("fetching data....."), 2000);
+  //const res = await fetch(`https://.../data`);
+  //const data = await res.json();
+
+  // Pass data to the page via props
+  //return { props: { data } };
+  return { props: {} };
+}
